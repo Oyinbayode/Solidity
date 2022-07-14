c7d8c2c37bf0251bb87f8351b3fd8f04d973b219
Oyinbayode <oyinbayode75@gmail.com>
2022-07-14 13:03:14 +0100

    interaction with ganache

--- Ethers-simple-storage/deploy.js
+++ Ethers-simple-storage/deploy.js
@@ -1,1 +1,31 @@
+const ethers = require("ethers");
+const fs = require("fs");
+require("dotenv").config();
 
+const main = async () => {
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const bytecode = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+
+  const contractFactory = new ethers.ContractFactory(abi, bytecode, wallet);
+  console.log("Deploying contract...");
+  const contract = await contractFactory.deploy();
+  await contract.deployTransaction.wait(1);
+
+  // GEt Number
+  const currentFavoriteNumber = await contract.retrieve();
+  console.log("Current favorite number:", currentFavoriteNumber.toString());
+
+  // Update Number
+  const transactionResponse = await contract.store("42");
+  await transactionResponse.wait(1);
+  const newFavoriteNumber = await contract.retrieve();
+  console.log("New favorite number:", newFavoriteNumber.toString());
+};
+
+main();
