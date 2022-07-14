992f49fbccbffc3b371d50b0e057994b9611647b
Oyinbayode <oyinbayode75@gmail.com>
2022-07-14 11:55:13 +0100

    first sol compilation

---
+++ Ethers-simple-storage/SimpleStorage.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract SimpleStorage {
+    uint256 public myNumber;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    struct People {
+        uint32 Age;
+        string FirstName;
+    }
+
+    People[] public people;
+
+    function addPerson(string memory _FirstName, uint32 _Age) public {
+        people.push(People(_Age, _FirstName));
+    }
+
+    function store(uint256 _myNumber) public virtual {
+        myNumber = _myNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return myNumber;
+    }
+}
